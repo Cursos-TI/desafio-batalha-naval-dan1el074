1cf5e163e3df976e9604b74b05c0f679343a2151
Daniel Vargas <daniel.rvargas10@gmail.com>
2025-04-12 15:22:29 +0000

    DESAFIO - novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,100 @@
 #include <stdio.h>
 
 // Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+// Nível Novato - Posicionamento dos Navios
+
+void desenhaTabuleiro(int tabuleiro[10][10]) {
+    char letra = 'A';
+
+    printf("\n   \033[32m");
+
+    for(int i=0; i<10; i++) {
+        printf("%c ", letra);
+        letra++;
+    }
+
+    for(int i=0; i<10; i++) {
+        i < 9 ? printf("\n ") : printf("\n");
+        printf("\033[32m%d \033[37m", i+1);
+
+        for(int j=0; j<10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+    }
+
+    printf("\n");
+}
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int navioX[2];
+    int navioY[2];
+    int inputNumero;
+    char inputLetra;
+    int tabuleiro[10][10] = {
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}
+    };
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    printf("\n --- Batalha Naval ---\n\n");
+    desenhaTabuleiro(tabuleiro);
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    // navioX
+    do {
+        printf("\nCoordenadas do navio horizontal:\nX [A-J]: ");
+        scanf(" %c", &inputLetra);
+    } while(inputLetra < 'A' || inputLetra > ('J' - 2));
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    do {
+        printf("Y [1-10]: ");
+        scanf(" %d", &inputNumero);
+    } while(inputNumero < 1 || inputNumero > 10);
+
+    navioX[0] = inputNumero - 1;
+    navioX[1] = (int) inputLetra - 65;
+
+    for(int i=0; i<3; i++) {
+        tabuleiro[navioX[0]][navioX[1] + i] = 3;
+    }
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    // navioY
+    inicioNavioY:
+    
+    do {
+        printf("\nCoordenadas do navio vertical:\nX [A-J]: ");
+        scanf(" %c", &inputLetra);
+    } while(inputLetra < 'A' || inputLetra > 'J');
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    do {
+        printf("Y [1-10]: ");
+        scanf(" %d", &inputNumero);
+    } while(inputNumero < 1 || inputNumero > (10 - 2));
 
+    navioY[0] = inputNumero - 1;
+    navioY[1] = (int) inputLetra - 65;
+
+    for(int i=0; i<3; i++) {
+        if(tabuleiro[navioY[0] + i][navioY[1]] != 0) {
+            for(int j=i; j>0; j--) {
+                tabuleiro[navioY[0] + (j - 1)][navioY[1]] = 0;
+            }
+
+            printf("Não pode haver colisão!\n");
+            goto inicioNavioY;
+        }
+
+        tabuleiro[navioY[0] + i][navioY[1]] = 3;
+    }
+
+    desenhaTabuleiro(tabuleiro);
+
+    printf("\n");
     return 0;
 }
